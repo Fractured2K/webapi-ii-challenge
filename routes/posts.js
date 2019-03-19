9b3392df5361f6682a4e4a106e016ec2ed3fd1ac
Skyelar Carroll <32372095+Fractured2K@users.noreply.github.com>
2019-03-19 11:29:41 -0700

    Create post endpoint error handling

--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,16 @@
 const router = require("express").Router();
 
 // Create post
-router.post("/", async (req, res) => {});
+router.post("/", async (req, res) => {
+	try {
+		const post = req.body;
+
+		// check for empty post title or contents
+		if (!post.title || !post.contents)
+			return res.status(400).json({
+				errorMessage: "Please provide title and contents for the post."
+			});
+	} catch (err) {}
+});
 
 module.exports = router;
